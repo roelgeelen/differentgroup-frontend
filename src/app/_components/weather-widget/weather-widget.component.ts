e5fa2f82edfed0d2556a72aa27bcd1a9598a40ac
R. Geelen <roel-geelen@hotmail.com>
2022-05-10 10:34:55 +0200

    update weather widget

--- src/app/_components/weather-widget/weather-widget.component.ts
+++ src/app/_components/weather-widget/weather-widget.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {delay, Observable} from "rxjs";
 import {WeatherService} from "../../_services/weather.service";
 
 @Component({
@@ -12,11 +12,27 @@ export class WeatherWidgetComponent implements OnInit {
   data: { name: string, image: string, main: any, dt: number, dt_txt: string; sys: any; wind: any; weather: any[]};
   dataForecast: any;
   loading = false;
+  loadingForecast = false;
+  id: number;
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
+    this.getWeather();
+    this.id = setInterval(() => {
+      this.getWeather();
+    }, 300000)
+  }
+
+  ngOnDestroy() {
+    if (this.id) {
+      clearInterval(this.id);
+    }
+  }
+
+  getWeather() {
     this.loading = true;
+    this.loadingForecast = true;
     this.weatherService.getWeatherForCity().subscribe(data => {
       this.data = data;
       this.loading = false;
@@ -24,8 +40,10 @@ export class WeatherWidgetComponent implements OnInit {
     // @ts-ignore
     this.weatherService.getWeatherForecast().subscribe((data: { list: any[] }) => {
       this.dataForecast = data.list.filter(function (d) {
-        return d.dt_txt.split(' ')[1].split(':')[0] === '12'
+        return d.dt_txt.split(' ')[1].split(':')[0] === '15'
       });
+      this.loadingForecast = false;
     })
   }
+
 }
