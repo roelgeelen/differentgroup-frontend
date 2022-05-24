d639f4a12d6ca0ba8040818ec9bfa37d56a02ced
R. Geelen <roel-geelen@hotmail.com>
2022-05-24 14:56:12 +0200

    update bday widget

--- src/app/_components/birthday-widget/birthday-widget.component.ts
+++ src/app/_components/birthday-widget/birthday-widget.component.ts
@@ -1,6 +1,9 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, ElementRef, Inject, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from "../../_services/api.service";
 import {Event} from "../../_models/calendar/Event";
+// @ts-ignore
+import * as confetti from "canvas-confetti";
+
 
 @Component({
   selector: 'app-birthday-widget',
@@ -8,22 +11,53 @@ import {Event} from "../../_models/calendar/Event";
   styleUrls: ['./birthday-widget.component.scss']
 })
 export class BirthdayWidgetComponent implements OnInit {
+  @ViewChild('bdayElem') el:ElementRef;
+  timer: any;
   loading = false;
   bdays: Event[] = [];
   error: string;
+  myConfetti: confetti;
 
   constructor(@Inject(ApiService)private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.loading = true
     this.apiService.getBirthdays().subscribe(e => {
-      console.log(e);
       this.bdays = e
+      if (this.isToday(this.bdays[0].start.dateTime)) {
+        this.timer = this.showConfetti();
+      }
       this.loading = false;
     }, error => {
       this.error = "Je hebt nog geen toegang tot deze kalender";
       this.loading = false;
     });
+    this.myConfetti = confetti.create(this.el, {
+      resize: true
+    });
   }
+
+  isToday(inputDate: Date) {
+     return (new Date(inputDate)).setHours(0,0,0,0) === (new Date()).setHours(0,0,0,0)
+  }
+
+  ngOnDestroy(){
+    clearInterval(this.timer);
+  }
+
+  showConfetti() {
+    return setInterval(() => {
+      // since particles fall down, start a bit higher than random
+      // @ts-ignore
+      this.myConfetti(Object.assign({}, { startVelocity: 25, spread: 360, ticks: 150, zIndex: 1 }, { particleCount: 100, origin: { x: this.randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+      this.myConfetti(Object.assign({}, { startVelocity: 25, spread: 360, ticks: 150, zIndex: 1 }, { particleCount: 100, origin: { x: this.randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 3000);
+  }
+
+  randomInRange(min: any, max: any) {
+    return Math.random() * (max - min) + min;
+  }
+
+
 
 }
