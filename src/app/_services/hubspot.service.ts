a8c2f32fb328a7f1988a991cc503dd2e889d5e17
R. Geelen <roel-geelen@hotmail.com>
2022-09-21 10:19:57 +0200

    update server

--- src/app/_services/hubspot.service.ts
+++ src/app/_services/hubspot.service.ts
@@ -14,42 +14,42 @@ export class HubspotService {
   }
 
   getDeal(dealId: number) {
-    return this.http.get<Deal>(`${environment.apiUrlTest}/deals/${dealId}`);
+    return this.http.get<Deal>(`${environment.apiUrl}/deals/${dealId}`);
   }
 
   updateDeal(dealId: number, deal: Object) {
-    return this.http.put(`${environment.apiUrlTest}/deals/${dealId}`, deal);
+    return this.http.put(`${environment.apiUrl}/deals/${dealId}`, deal);
   }
 
   getDealConfigs(dealId: number) {
-    return this.http.get<DealConfig>(`${environment.apiUrlTest}/deals/${dealId}/configs`);
+    return this.http.get<DealConfig>(`${environment.apiUrl}/deals/${dealId}/configs`);
   }
 
   createDealConfig(dealId: number, deal: DealConfig) {
-    return this.http.post(`${environment.apiUrlTest}/deals/${dealId}/configs/create`, deal);
+    return this.http.post(`${environment.apiUrl}/deals/${dealId}/configs/create`, deal);
   }
 
   getConfig(dealId: number, configId: number) {
-    return this.http.get<DealConfig>(`${environment.apiUrlTest}/deals/${dealId}/configs/${configId}`);
+    return this.http.get<DealConfig>(`${environment.apiUrl}/deals/${dealId}/configs/${configId}`);
   }
 
   updateDealConfig(dealId: number, configId: number, deal: DealConfig) {
-    return this.http.put(`${environment.apiUrlTest}/deals/${dealId}/configs/${configId}`, deal);
+    return this.http.put(`${environment.apiUrl}/deals/${dealId}/configs/${configId}`, deal);
   }
 
   deleteDealConfig(dealId: number, configId: number) {
-    return this.http.delete(`${environment.apiUrlTest}/deals/${dealId}/configs/${configId}`);
+    return this.http.delete(`${environment.apiUrl}/deals/${dealId}/configs/${configId}`);
   }
 
   createInvoice(dealId: number, configId: number, replace: boolean, values: string[]) {
-    return this.http.post(`${environment.apiUrlTest}/deals/${dealId}/configs/${configId}/invoice?replace=${replace}`, values);
+    return this.http.post(`${environment.apiUrl}/deals/${dealId}/configs/${configId}/invoice?replace=${replace}`, values);
   }
 
   saveImage(dealId: number, configId: number, file: File, filename: string): Observable<HttpEvent<{}>> {
     const formdata: FormData = new FormData();
     formdata.append('file', file);
     formdata.append('filename', filename);
-    const req = new HttpRequest('POST', `${environment.apiUrlTest}/deals/${dealId}/configs/${configId}/upload/image`, formdata, {
+    const req = new HttpRequest('POST', `${environment.apiUrl}/deals/${dealId}/configs/${configId}/upload/image`, formdata, {
       reportProgress: true,
       responseType: 'text'
     });
