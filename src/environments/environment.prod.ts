4a8196aae8d5e6ea5af68c9e532cfb253759b26b
R. Geelen <roel-geelen@hotmail.com>
2023-01-06 13:58:40 +0100

    update server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl:"https://api.differentgroup.nl/differentgroup",
-  apiUrlLegacy:"https://api.differentdoors.nl/v2/api",
+  apiUrlLegacy:"https://api.differentgroup.nl/differentgroup",
 };
